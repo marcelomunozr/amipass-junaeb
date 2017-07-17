027c5bd01f19e692d4c452367ead8094cb7d973f
Marcelo <Marcelo>
2017-07-17 18:06:05 -0400

    subida

---
+++ README.md
@@ -0,0 +1,1 @@
+# [Amipass Junaeb](http://www.becajunaebamipass.com/)
---
+++ js/custom.js
@@ -0,0 +1,11 @@
+
+$(function() {
+  $('.slick-homi').slick({
+    prevArrow: ".le-prev",
+    nextArrow: ".le-next"
+  });
+  $('.slick-faq').slick({
+    prevArrow: ".el-prev",
+    nextArrow: ".el-next"
+  });
+});
